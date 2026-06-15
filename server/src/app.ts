a0bc8f0bd4ebf7606c820d5ed7969096038023db
oladojabasit29@gmail.com <oladojabasit29@gmail.com>
2026-06-15 11:16:11 +0100

    Removed catch-all path to fix express 5

--- server/src/app.ts
+++ server/src/app.ts
@@ -27,7 +27,7 @@ app.use("/api/auth", authRoutes);
 
 app.use("/api/transactions", transactionRoutes);
 
-app.use("/:path(.*)", (_, res) => {
+app.use((_, res) => {
     res.status(404).json({
         message: "Route not found"
     })
