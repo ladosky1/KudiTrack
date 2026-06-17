039b989800e30854aa656d708ebced36c9e38bc6
oladojabasit29@gmail.com <oladojabasit29@gmail.com>
2026-06-17 18:19:41 +0100

    added api base url prefix and vercel router

--- frontend/src/services/api/client.ts
+++ frontend/src/services/api/client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const apiClient = axios.create({
     baseURL: 
-        import.meta.env.VITE_API_URL,
+        `${import.meta.env.VITE_API_URL}/api`,
 
     withCredentials: true,
 });
